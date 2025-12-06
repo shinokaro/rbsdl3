adb8c05e4eedc78f43b9bb4aab958f86ab7bd64b
shinokaro <shinokaro@hotmail.co.jp>
2025-12-06 20:25:35 +0900

    Eagerly include SDL3::Bindings at module load
    - Require "rb_sdl3/sdl3/bindings" upfront and include Bindings into RbSDL3::SDL3 at load time so that SDL API methods and constants are available before dlload.
    - Change SDL3.dlload to only call super and then notify Bindings via Bindings.included(self), keeping the runtime library loading semantics unchanged.

--- lib/rb_sdl3/sdl3.rb
+++ lib/rb_sdl3/sdl3.rb
@@ -1,16 +1,17 @@
 # frozen_string_literal: true
 
 require "fiddle/import"
+require_relative "sdl3/bindings"
 
 module RbSDL3
   module SDL3
     extend Fiddle::Importer
+    dlload
+    include Bindings
 
     def self.dlload(*libs)
       super
-
-      require_relative "sdl3/bindings"
-      include Bindings
+      Bindings.included(self)
     end
   end
 end
