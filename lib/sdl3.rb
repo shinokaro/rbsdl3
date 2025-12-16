11a3933a994b9202bbd10c1ad416e98961eec5ce
shinokaro <shinokaro@hotmail.co.jp>
2025-12-17 08:22:39 +0900

    Centralize Fiddle::Importer dlload in SDL3 and inject bindings from submodules
    SDL3 now extends Fiddle::Importer and becomes the single dlload entry point.
    Submodule bindings are injected into SDL3 via included hooks; submodules are
    marked as private constants and act as binding injectors rather than API
    namespaces.

--- lib/sdl3.rb
+++ lib/sdl3.rb
@@ -1,5 +1,14 @@
 # frozen_string_literal: true
+require "fiddle/import"
 require_relative "sdl3/version"
 
 module SDL3
+  extend Fiddle::Importer
+
+  def self.dlload(*libs)
+    super
+    const_get(:SDL, false).included(self) if const_defined?(:SDL, false)
+  end
+
+  dlload
 end
