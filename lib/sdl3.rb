539fbc7662d26f0f48a7dfaeffc75fe848e19564
shinokaro <shinokaro@hotmail.co.jp>
2025-12-18 11:36:57 +0900

    Generalize dlload re-include targets
    Introduce DLLOAD_TARGETS and iterate it in SDL3.dlload to re-include managed
    submodules after dlload, preparing for additional modules (e.g. Image).

--- lib/sdl3.rb
+++ lib/sdl3.rb
@@ -5,9 +5,14 @@ require_relative "sdl3/version"
 module SDL3
   extend Fiddle::Importer
 
+  DLLOAD_TARGETS = %i[SDL].freeze
+  private_constant :DLLOAD_TARGETS
+
   def self.dlload(*libs)
     super
-    const_get(:SDL, false).included(self) if const_defined?(:SDL, false)
+    DLLOAD_TARGETS.each do |m|
+      const_get(m, false).included(self) if const_defined?(m, false)
+    end
   end
 
   dlload
